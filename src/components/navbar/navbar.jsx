2642bbcb5122cc17606da34a37f447c731c5ae7a
helenm1 <helenmiaoo@gmail.com>
2023-02-11 17:05:06 -0500

    centered logo

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,13 +1,26 @@
 
 import React from "react";
+import "./navbar.css";
 
 export function NavBar() {
+
     return (
-        <div>
-            <img src="../images/shrimpPic.png"/>
-            <h3>
+
+        
+        <div class = "container">
+            <div>
+            <img src="/images/shrimpPic.png" alt="shrimp" width="35" height="35"/>
+
+            <h3 class = "rgLogo">
                 RG
             </h3>
+            
+            </div>
+            
         </div>
+        
+
+        
     );
+
 }
