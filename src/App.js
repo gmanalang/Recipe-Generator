1ee93c145f721d24f06aa071b616ba8e47406235
helenm1 <helenmiaoo@gmail.com>
2023-02-11 15:37:30 -0500

    navbar stuff

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { NavBar } from './components/navbar/navbar.jsx'
 
 function App() {
   return (
     <div className='parent'>
+      <NavBar>
+
+      </NavBar>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+export function NavBar() {
+    return (
+        <div>
+            <img src="../images/shrimpPic.png"> </img>
+            <h3>
+                RG
+            </h3>
+        </div>
+    );
+}
+
+//export default NavBar;
