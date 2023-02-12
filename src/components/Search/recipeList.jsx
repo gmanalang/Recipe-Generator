11673b48652cc8a77d67d78040c3dc24223206b8
Angela Zheng <zheng.ang@northeastern.edu>
2023-02-11 20:15:43 -0500

    stuff

--- src/components/Search/recipeList.jsx
+++ src/components/Search/recipeList.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
-export function RecipeList({title, cuisineType, image, ingredients}) {
-    return(
-        <div className>
-            <h1>{title}</h1>
-            <ol>
-                <h1>Ingredients</h1>
-                {ingredients.map(ingredient =>(
-                    <li>{ingredient.text}</li>
-                ))}
-            </ol>
-            <img src={image} alt=""/>
-            <p>Cuisine: {cuisineType}</p>
-        </div>
-    );
-};
+export function RecipeList({ title, cuisineType, image, ingredients }) {
+  return (
+    <div>
+      <div className="card">
+        <h1>{title}</h1>
+        <ol>
+          <h1 id="title">Ingredients</h1>
+          {ingredients.map((ingredient) => (
+            <li>{ingredient.text}</li>
+          ))}
+        </ol>
+        <img src={image} alt="" />
+        <p>Cuisine: {cuisineType}</p>
+      </div>
+    </div>
+  );
+}
